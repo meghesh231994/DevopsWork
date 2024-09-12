fb3b9a322bbdb766e8233a6acdc71b4333b3629e
meghesh231994 <82071782+meghesh231994@users.noreply.github.com>
2024-09-12 12:04:57 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -9,36 +9,16 @@ variable "instance_type" {
 }
 
 variable "ami_id" {
-  default = "ami-0e86e20dae9224db8"  
-}
-
-# Create a VPC
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-  
-  tags = {
-    Name = "main-vpc"
-  }
-}
-
-# Create a subnet
-resource "aws_subnet" "main" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
-  
-  tags = {
-    Name = "main-subnet"
-  }
+  default = "ami-0e86e20dae9224db8"  # Amazon Linux 2 AMI, update as needed
 }
 
 # Create a security group
 resource "aws_security_group" "allow_ssh_http" {
   name        = "allow_ssh_http"
   description = "Allow SSH and HTTP inbound traffic"
-  vpc_id      = aws_vpc.main.id
 
   ingress {
-    description = "SSH from VPC"
+    description = "SSH from anywhere"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
@@ -46,7 +26,7 @@ resource "aws_security_group" "allow_ssh_http" {
   }
 
   ingress {
-    description = "HTTP from VPC"
+    description = "HTTP from anywhere"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
@@ -69,7 +49,7 @@ resource "aws_security_group" "allow_ssh_http" {
 resource "aws_instance" "sonarqube" {
   ami           = var.ami_id
   instance_type = var.instance_type
-  subnet_id     = aws_subnet.main.id
+  associate_public_ip_address = true
   vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
 
   tags = {
@@ -87,7 +67,7 @@ resource "aws_instance" "sonarqube" {
 resource "aws_instance" "test" {
   ami           = var.ami_id
   instance_type = var.instance_type
-  subnet_id     = aws_subnet.main.id
+  associate_public_ip_address = true
   vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
 
   tags = {
@@ -105,7 +85,7 @@ resource "aws_instance" "test" {
 resource "aws_instance" "nexus" {
   ami           = var.ami_id
   instance_type = var.instance_type
-  subnet_id     = aws_subnet.main.id
+  associate_public_ip_address = true
   vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
 
   tags = {
