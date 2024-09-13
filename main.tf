66850332b779a3daf922aa4e625f2d233e00ef4f
meghesh231994 <82071782+meghesh231994@users.noreply.github.com>
2024-09-13 11:23:10 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -3,19 +3,23 @@ provider "aws" {
   region = "us-east-1"  # Change this to your desired region
 }
 
-# Define variables
-variable "instance_type" {
-  default = "t2.medium"  # Adjust instance type as needed
+# Data source to get the default VPC
+data "aws_vpc" "default" {
+  default = true
 }
 
-variable "ami_id" {
-  default = "ami-0e86e20dae9224db8"  # Amazon Linux 2 AMI, update as needed
+# Data source to get the default subnet in the first availability zone
+data "aws_subnet" "default" {
+  vpc_id            = data.aws_vpc.default.id
+  availability_zone = "${data.aws_vpc.default.region}a"
+  default_for_az    = true
 }
 
 # Create a security group
 resource "aws_security_group" "allow_ssh_http" {
   name        = "allow_ssh_http"
   description = "Allow SSH and HTTP inbound traffic"
+  vpc_id      = data.aws_vpc.default.id
 
   ingress {
     description = "SSH from anywhere"
@@ -45,69 +49,20 @@ resource "aws_security_group" "allow_ssh_http" {
   }
 }
 
-# Create SonarQube EC2 instance
-resource "aws_instance" "sonarqube" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  associate_public_ip_address = true
+# Create EC2 instances
+resource "aws_instance" "example" {
+  count                  = 3
+  ami                    = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 AMI, update as needed
+  instance_type          = "t2.micro"
+  subnet_id              = data.aws_subnet.default.id
   vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
 
   tags = {
-    Name = "SonarQube-Server"
+    Name = "Example-Instance-${count.index + 1}"
   }
-
-  user_data = <<-EOF
-              #!/bin/bash
-              # Install SonarQube
-              # Add your SonarQube installation commands here
-              EOF
-}
-
-# Create Test EC2 instance
-resource "aws_instance" "test" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  associate_public_ip_address = true
-  vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
-
-  tags = {
-    Name = "Test-Server"
-  }
-
-  user_data = <<-EOF
-              #!/bin/bash
-              # Configure Test server
-              # Add your Test server configuration commands here
-              EOF
-}
-
-# Create Nexus EC2 instance
-resource "aws_instance" "nexus" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  associate_public_ip_address = true
-  vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
-
-  tags = {
-    Name = "Nexus-Server"
-  }
-
-  user_data = <<-EOF
-              #!/bin/bash
-              # Install Nexus
-              # Add your Nexus installation commands here
-              EOF
 }
 
 # Output the public IPs of the instances
-output "sonarqube_public_ip" {
-  value = aws_instance.sonarqube.public_ip
-}
-
-output "test_public_ip" {
-  value = aws_instance.test.public_ip
-}
-
-output "nexus_public_ip" {
-  value = aws_instance.nexus.public_ip
+output "instance_public_ips" {
+  value = aws_instance.example[*].public_ip
 }
